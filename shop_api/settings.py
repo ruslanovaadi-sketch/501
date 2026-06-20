0dc284bcc95308b5274a1c85895efc78b78b4eb5
ruslanovaadi-sketch <ruslanovaadi@gmail.com>
2026-06-20 11:36:40 +0600

    first commit

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -78,6 +78,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
+    
     }
 }
 
